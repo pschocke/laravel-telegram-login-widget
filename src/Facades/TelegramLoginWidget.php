52e71bda26e085e97e4f3eaafbd153b385c1a2f4
KuebelELch15 <schockinio@gmail.com>
2020-02-23 21:29:08 +0100

    added phpdoc for facade

--- src/Facades/TelegramLoginWidget.php
+++ src/Facades/TelegramLoginWidget.php
@@ -2,8 +2,14 @@
 
 namespace pschocke\TelegramLoginWidget\Facades;
 
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class TelegramLoginWidget
+ * @method static Collection validateResponse(array|Request|Collection $collection)
+ */
 class TelegramLoginWidget extends Facade
 {
     protected static function getFacadeAccessor()
